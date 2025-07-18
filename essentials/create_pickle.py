d469ea2b15c27548763139648c395b956397b5ba
d41y <d41y@d41y.com>
2025-07-18 04:35:13 +0200

    Add .gitignore rules

--- essentials/create_pickle.py
+++
@@ -1,7 +0,0 @@
-import pickle
-
-with open('.wordlist/german_nouns_5.txt', 'r', encoding='latin-1') as wordlist:
-    content = [line.strip() for line in wordlist]
-
-with open('pickled_wordlist.pkl', 'wb') as pickled:
-    pickle.dump(content, pickled)
